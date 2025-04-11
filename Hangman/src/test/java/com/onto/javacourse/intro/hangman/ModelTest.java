bdfe627a80d5980050c8ce99b80026fdfcfce013
Vik4o1212 <vikenchev@gmail.com>
2025-04-11 18:08:19 +0300

    #1 Package the code of the hangman task as a .jar file

---
+++ Hangman/src/test/java/com/onto/javacourse/intro/hangman/ModelTest.java
@@ -0,0 +1,36 @@
+package com.onto.javacourse.intro.hangman;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.onto.javacourse.intro.hangman.Model;
+
+import java.lang.reflect.Field;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ModelTest {
+    private Model model;
+
+    @BeforeEach
+    void setUp(){
+        model = new Model("cash");
+    }
+
+    @Test
+    void testModelCorrectSettingComputerWord() throws NoSuchFieldException, IllegalAccessException {
+        Field f = Model.class.getDeclaredField("COMPUTER_WORD");
+        f.setAccessible(true);
+
+        assertEquals("cash", f.get(model));
+    }
+
+    @Test
+    void testDoesContainLetterCorrectFindingLetter(){
+        assertTrue(model.doesContainLetter('a'));
+    }
+
+    @Test
+    void testDoWordsMatchCorrectIsMatchingAWord(){
+        assertTrue(model.doWordsMatch("cash"));
+    }
+}
